a3ac723d17524c001075e2119dfd36ae454a649e
Serkan Sahin <ssahin55@outlook.com>
2020-08-02 19:24:46 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,32 +1,59 @@
-katz_deli = [""]
-
 def line(queue_of_people)
   if queue_of_people.empty?
     puts "The line is currently empty."
   else
     message = "The line is currently:"
     queue_of_people.each_with_index do |name, index|
-      message = message + " #{index +1}. #{name}"
+      message = message + " #{index+1}. #{name}"
     end
-    puts message
-  end
-end
-
-
-def take_a_number(queue_of_people, name_at_end)
-  position = queue_of_people.length + 1
-  message = "Welcome, #{name_at_end}. You are number #{position} in line."
-  queue_of_people << name_at_end
-  puts message 
-end
-
-
-def now_serving (queue_of_people)
-  if queue_of_people.empty?
-    puts "There is nobody waiting to be served!"
-  else
-    first_person_name = queue_of_people.shift
-    message = "Currently serving #{first_person_name}."
     puts message 
   end
 end
+  
+  
+#=> "The line is currently empty."
+#=> ="The line is currently: 1. Grace 2. Kent 3. Matz"
+
+
+def take_a_number(queue_of_people, name_joining)
+  queue_of_people << name_joining
+  position = queue_of_people.length
+  message = "Welcome, #{name_joining}. You are number #{position} in line."
+  puts message
+end
+
+
+#Welcome, Ada. You are number 1 in line.
+
+def take_a_number(queue_of_people)
+  count = 0 
+  count += 1
+  
+  message = "Wecome, you are number #{count} in line."
+  queue_of_people << count
+  return message
+  
+end
+  
+
+
+#Welcome, you are number 1 in line.
+
+
+def now_serving(queue_of_people)
+  if queue_of_people.empty?
+    puts "There is nobody waiting to be served!"
+  else
+   
+   name_at_front = queue_of_people.shift
+   
+   message = "Currently serving #{name_at_front}."
+   puts message
+  end
+end
+
+
+
+
+#"There is nobody waiting to be served!"
+#"Currently serving Ada."
